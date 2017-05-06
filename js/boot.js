038614e8f88e39d26f9fc62d93fe4ce5cf5c5e34
1nevi <ziniewicz.p@gmail.com>
2017-05-06 21:57:16 +0200

    cleaning up

--- js/boot.js
+++ js/boot.js
@@ -9,8 +9,7 @@ Boot.prototype = {
 	},
 
   	create: function(){
-		//this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-
+        
         //  We're going to be using physics, so enable the Arcade Physics system
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
