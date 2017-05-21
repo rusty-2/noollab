c6d5f8d748fc95ec8ebeb2f52750f58788e70890
rusty-2 <tmszraus@gmail.com>
2017-05-22 00:10:19 +0200

    formatting

--- js/levels/level1.js
+++ js/levels/level1.js
@@ -1,11 +1,11 @@
-define([], function () {
+define([], function() {
     return {
         balloons: [{
-          level: 2,
-          pos: {
-            x:400-25/2,
-            y:200
-          }
+            level: 2,
+            pos: {
+                x: 400 - 25 / 2,
+                y: 200
+            }
         }]
     };
 });
--- js/levels/level2.js
+++ js/levels/level2.js
@@ -1,17 +1,17 @@
-define([], function () {
+define([], function() {
     return {
-      balloons: [{
-        level: 2,
-        pos: {
-          x:400-25/2,
-          y:200
-        }
-      }, {
-        level: 3,
-        pos: {
-          x:200-25/2,
-          y:300
-        }
-      }]
+        balloons: [{
+            level: 2,
+            pos: {
+                x: 400 - 25 / 2,
+                y: 200
+            }
+        }, {
+            level: 3,
+            pos: {
+                x: 200 - 25 / 2,
+                y: 300
+            }
+        }]
     };
 });
